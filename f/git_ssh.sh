16cc5fe511cf7807245d5568f6bc33232256a860
u0_a26 <u0_a26@localhost>
2025-12-22 15:27:41 +0100

    _Xiaomi_Mon Dec 22 15:27:41 CET 2025

--- f/git_ssh.sh
+++ f/git_ssh.sh
@@ -1,1 +1,11 @@
-git_ssh() { local FZF_DEFAULT_OPTS=; gggg="$(ls ~/.ssh|grep -vE '.pub|known_hosts|authorized_keys'|fzf --preview '(cat ~/.ssh/{}.pub|tr " " "\n"; echo; echo; cat ~/.ssh/{})|bat -ppflgo' -i --wrap --preview-window 'wrap' --wrap-sign '' --bind 'q:abort')"; printf %b "\n\n\e[A -- change git ssh-key to: $gggg ? [Y/n] "; read -sn1 ny; [ -z "$ny" ] && git config set core.sshCommand "ssh -i ~/.ssh/$gggg"; echo; }; 
+git_ssh() { local FZF_DEFAULT_OPTS=; gggg="$(ls ~/.ssh|grep -vE '.pub|known_hosts|authorized_keys'|fzf --preview '(cat ~/.ssh/{}.pub|tr " " "\n"; echo; echo; cat ~/.ssh/{})|bat -ppflgo' -i --wrap --preview-window 'wrap' --wrap-sign '' --bind 'q:abort')"; 
+
+printf %b "\n\n\e[A -- change git ssh-key to: $gggg ? [Y/n] "; read -sn1 ny; [ -z "$ny" ] && git config set core.sshCommand "ssh -i ~/.ssh/$gggg"; echo; 
+
+
+cat .git/config | sed -e "s/url\ \=\ https\:\/\/github.com\//url\ \=\ git@github.com:/"; 
+printf %b "\n\n\e[A -- change git.config to this ? [Y/n] "; read -sn1 lk; [ -z "$lk" ] && git config set core.sshCommand "ssh -i ~/.ssh/$gggg"; echo; 
+
+sed -i$(date +%m%d%H%M%S) -e "s/url\ \=\ https\:\/\/github.com\//url\ \=\ git@github.com:/" .git/config; 
+echo; 
+}; 
