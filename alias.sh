6633dec39acdb5822859e7c21e61690526107d55
aa <aa@ae>
2025-12-31 14:34:54 +0100

    aa_ASUSTeK_Wed Dec 31 02:34:54 PM CET 2025

--- alias.sh
+++ alias.sh
@@ -7,6 +7,7 @@ alias kk='fmt -g 66|col -xb|bat -pfljava --theme DarkNeon';
 alias kkkk='col -xb|fmt --split-only --width=$((COLUMNS - 2)) --goal=66 --uniform-spacing --tagged-paragraph|bat -pfljava --theme TwoDark'; 
 alias cccc='crontab -e'; 
 alias xxxx='ssh -p 8022 aa@192.168.0.19 "mpv $HOME/88/m/money.mp3"'
+alias gh_release_dl='gh release download "$(gh release list --limit 1|tail -n1|tr -s " \t" " "|cut -f1 -d" ")"'; 
 alias 12alias='$EDITOR ~/88/alias.sh'; 
 alias antsup='ssh aa@ants.ftp.sh "sudo apt update; sudo apt upgrade; sudo reboot"'; 
 alias s='ssss -sl4|grep -vE "UNCONN"|tr -s " " " "|cut -f1,2,5 -d" "|column --table --output-separator " : "|bat -ppfld'; 
