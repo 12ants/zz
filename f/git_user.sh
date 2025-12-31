831492389f1742e747081a47e946e57a58358290
aa <aa@ae>
2025-12-31 05:58:38 +0100

    aa_ASUSTeK_Wed Dec 31 05:58:38 AM CET 2025

---
+++ f/git_user.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+function git_user () { 
+####
+IFS=$'\n\t '; 
+ghusers=($(gh auth status|col -xb|grep -e "Logged in"|cut -f9 -d" ")); 
+uhw="$(printf %b "${ghusers[*]}"|wc --max-line-length)"; 
+####
+ghuser1="$(printf %b "${ghusers[*]}"|command fzf-tmux -h "$((${#ghusers[*]} +3))" -w "$((uhw + 6))")"; 
+####
+####
+printf %b "\n"; 
+gh auth switch -u "${ghuser1}"; 
+printf %b "\n"; 
+####
+git config --global core.sshCommand "ssh -i ~/.ssh/gh_$ghuser1"; 
+git config --list --global|bat -ppflc; 
+####
+}; 
