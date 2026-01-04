95aff233f0b84e7dab043a27b4dfcbbc787b6cd4
aa <aa@ae>
2026-01-04 14:51:44 +0100

    aa_ASUSTeK_Sun Jan  4 02:51:44 PM CET 2026

--- f/mnu.sh
+++ f/mnu.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 function mnu () { 
-local IFS=$'\n\t '; unset -v uhl uhw mm; 
-mnu=(${@}); [ -z "$1" ] && mnu=($(command ls -1p|head -n $((LINES - 4)))); 
+IFS=$'\n\t '; unset -v uhl uhw mm; 
+mnu=(${@}); [ -z "$1" ] && \
+mnu=($(command ls -1p|head -n $((LINES - 4)))); 
 uhl="${#mnu[*]}"; uhw=$(printf %b "${mnu[*]}"|wc --max-line-length); 
-mm=($(printf %b "${mnu[*]}"|command fzf-tmux -w "$((uhw + 6))" -h "$((uhl + 3))" -m --cycle --ansi --bind 'enter:print-query' --disabled --info inline:"" --bind 'focus:replace-query,q:abort')); 
-printf %b "${mm[*]}\n"; }; 
+########
+########
+########
+declare -a mm=($(printf %b "${mnu[*]}"|command fzf-tmux -w "$((uhw + 6))" -h "$((uhl + 3))" -m --cycle --ansi --bind 'enter:print-query' --disabled --info inline:"" --bind 'focus:replace-query,q:abort')); ####
+####
+printf %b "${mm[*]} \n"; 
+####
+}; 
