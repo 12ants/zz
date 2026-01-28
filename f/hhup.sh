3470e740149a6b5e3477d665625e439686fafad5
u0_a26 <u0_a26@localhost>
2026-01-28 01:41:32 +0100

    _Xiaomi_Wed Jan 28 01:41:32 CET 2026

---
+++ f/hhup.sh
@@ -0,0 +1,1 @@
+hhup() { mt=$(mktemp); tail -n 88 $HISTFILE|fzf --tac --height "~5%" --border none --info inline-right --preview-window "hidden" --no-separator --no-scrollbar --color "bg:234,bg+:29" --no-unicode --freeze-left $((COLUMNS/12)) --freeze-right $((COLUMNS/18)) --pointer "" --ellipsis "" > $mt; chmod +x $mt; history -s "$(cat $mt)"; }; 
