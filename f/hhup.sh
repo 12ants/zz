f8bcac6c8561ae28d432ff8051320fb92b53872c
u0_a26 <u0_a26@localhost>
2026-01-28 12:20:24 +0100

    _Xiaomi_Wed Jan 28 12:20:24 CET 2026

--- f/hhup.sh
+++ f/hhup.sh
@@ -1,1 +1,11 @@
-hhup() { mt=$(mktemp); tail -n 88 $HISTFILE|fzf --tac --height "~5%" --border none --info inline-right --preview-window "hidden" --no-separator --no-scrollbar --color "bg:234,bg+:29" --no-unicode --freeze-left $((COLUMNS/12)) --freeze-right $((COLUMNS/18)) --pointer "" --ellipsis "" > $mt; chmod +x $mt; history -s "$(cat $mt)"; }; 
+hhup() { mt=$(mktemp); tail -n 88 $HISTFILE|fzf --tac --height "~5%" --border none --info inline-right --preview-window "hidden" --no-separator --no-scrollbar --color "bg:234,bg+:29" --no-unicode --freeze-left $((COLUMNS/12)) --freeze-right $((COLUMNS/18)) --pointer "" --ellipsis "" > $mt; chmod +x $mt; 
+history -s "$(cat $mt)"; 
+history -w; 
+# IFS=$'\n\t '; 
+read -rei "$(tail -n1 $HISTFILE)" ssss; 
+eval "$ssss"; 
+# history -w; 
+# read -ei "$BASH_COMMAND" ssss; 
+# history -s "$(tail -n1 "$HISTFILE")"; 
+# read -ei "$(tail -n1 "$HISTFILE")" "ssss" && ${ssss} || echo ok; 
+}; 
---
+++ s/hs.sh
@@ -0,0 +1,2 @@
+ssss="$(tail -n 88 ${HISTFILE-1}|fzf)"; 
+bash -c "$ssss"; 
