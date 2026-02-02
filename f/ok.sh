2d8e14891bfa3d31dd2bb8a6b58df2a9db48f9bb
aa <aa@ae>
2026-02-03 00:49:05 +0100

    aa_ASUSTeK_Tue Feb  3 12:49:05 AM CET 2026

--- f/ok.sh
+++ f/ok.sh
@@ -1,12 +1,16 @@
-ok() { wc="$(figlet -tcf rebel "${*-ok}"|wc -l)"; 
+ok() { 
+####
+wc="$(figlet -tcf rebel "${*-ok} "|wc -l)"; 
+####
 fifi() { rr1=$((RANDOM%222)); rr2=$((RANDOM%222)); 
-figlet -tcf rebel "${*} "|sed -e "s/█/\x1b[38;5;${rr1}m█\x1b[0m/g" -e "s/▒/\x1b[38;5;${rr2}m▒\x1b[0m/g"; }; 
+figlet -tcf rebel "${*}"|sed -e "s/█/\x1b[38;5;${rr1}m█\x1b[0m/g" -e "s/▒/\x1b[38;5;${rr2}m▒\x1b[0m/g"; }; 
 ####
 for i in $(seq $wc); do echo; done; 
-printf %b "\e[?25l"; 
+printf %b "\n\n\e[?25l"; 
 for i in {1..22}; do 
 printf %b "\e[${wc}A"; 
-fifi "${*-ok}"; 
-read -sn1 -t .1; 
+fifi "${*-ok}"; read -sn1 -t .1; 
 [ $? != 142 ] && break; 
-done; printf %b "\e[?25h"; }; 
+done; printf %b "\e[?25h"; 
+####
+}; 
