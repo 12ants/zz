6b29f1e2b2d56f816b440efcab6f28bca158faf7
aa <aa@ae>
2026-02-03 00:43:18 +0100

    aa_ASUSTeK_Tue Feb  3 12:43:18 AM CET 2026

--- f/ok.sh
+++ f/ok.sh
@@ -1,2 +1,12 @@
-ok() { printf %b "\e[?25l\n\n\n\n\n\n\n\n\e[5A"; for i in {1..22}; do fifi "${*-ok}"; read -sn1 -t .1; [ $? = 142 ] && printf %b "\e[11A" || break; done; printf %b "\e[?25h"; fifi() { rr1=$((RANDOM%222)); rr2=$((RANDOM%222)); figlet -tcf rebel ${1-$(date)}|sed -e "s/█/\x1b[38;5;${rr1}m█\x1b[0m/g" -e "s/▒/\x1b[38;5;${rr2}m▒\x1b[0m/g"; }; }; 
-
+ok() { wc="$(figlet -tcf rebel "${*-ok}"|wc -l)"; 
+fifi() { rr1=$((RANDOM%222)); rr2=$((RANDOM%222)); 
+figlet -tcf rebel "${*} "|sed -e "s/█/\x1b[38;5;${rr1}m█\x1b[0m/g" -e "s/▒/\x1b[38;5;${rr2}m▒\x1b[0m/g"; }; 
+####
+for i in $(seq $wc); do echo; done; 
+printf %b "\e[?25l"; 
+for i in {1..22}; do 
+printf %b "\e[${wc}A"; 
+fifi "${*-ok}"; 
+read -sn1 -t .1; 
+[ $? != 142 ] && break; 
+done; printf %b "\e[?25h"; }; 
