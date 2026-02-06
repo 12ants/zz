1e437eda28b3676d19697d9cdf6d734179590974
u0_a26 <u0_a26@localhost>
2026-02-06 02:55:15 +0100

    _Xiaomi_Fri Feb  6 02:55:15 CET 2026

--- s/wcodes.sh
+++ s/wcodes.sh
@@ -29,7 +29,16 @@ wc[25]="Light snowfall";
 wc[26]="Moderate snowfall"; 
 wc[27]="Heavy snowfall"; 
 ####
-curl -sL "https://opendata-download-metfcst.smhi.se/api/category/snow1g/version/1/geotype/point/lon/16.158/lat/58.5812/data.json?timeseries=12?parameters=air_temperature,symbol_code,wind_speed" -o "$HOME/logs/tmp/wc.json"
+curl -sL "https://opendata-download-metfcst.smhi.se/api/category/snow1g/version/1/geotype/point/lon/16.158/lat/58.5812/data.json?timeseries=45?parameters=air_temperature,symbol_code" -o "$HOME/logs/wc.json"
 
 
-cat wc.json |tr -s "[]{}," "\n\"\n"|tail -n+10|grep -vE "inter|data"|sed -e "s/\:00\:00Z//g"|cut -f2- -d":"|sed -e "s/T/Twt[/g"|cut -f2 -d"T"|tr "\n\"" " _"|sed -e "s/_\ /]=(/g" -e "s/\.00 /);\n/g"; 
+# cat $HOME/logs/wc.json |tr -s "[]{}," "\n\"\n"|tail -n+10|grep -vE "inter|data"|sed -e "s/\:00\:00Z//g"|cut -f2- -d":"|sed -e "s/T/Twt[/g"|cut -f2 -d"T"|tr "\n\"" " _"|sed -e "s/_\ /]=(/g" -e "s/\.00 /);\n/g"; 
+
+IFS=$'\n' wttr=($(cat $HOME/logs/wc.json |tr -s "[]{}, " "\n"|tail -n+10|grep -vE "inter|data|time"|cut -f2 -d":"|tr -s "\n" " "|sed -e 's/\.00/\n/g'|sed -e 's/^\ //g')); 
+##
+eval $(kk=0; for i in ${!wttr[*]}; do kk=$((kk+3600)); date --date=@$(($(date +%s)+$kk)) +%h\|%d\ %R; printf %b "${wttr[i]}\n__\n"; done|tr -s "\n" " "|tr -s "_" "\n"|sed -e 's/^\ //g'|sed -e 's/^/echo\ \-e\ \"/g' -e 's/\ /\ \$\{wc\[/5' -e 's/\ /\ \[/4' -e 's/\ /c\]\ /5' -e 's/.$/\]\}\";\ \n\n/g')|bat -ppflnim --theme Nord; 
+##
+
+# eval $(kk=0; for i in ${!wttr[*]}; do kk=$((kk+3600)); date --date=@$(($(date +%s)+$kk)) +%h\ %d\ \-\ %R; printf %b "${wttr[i]}\n__\n"; done|tr -s "\n" " "|tr -s "_" "\n"|sed -e 's/^\ //g'|sed -e 's/^/echo\ \-e\ \"/g' -e 's/\ /\ \$\{wc\[/7' -e 's/.$/\]\}\";\ \n\n/g'); 
+
+# kk=0; for i in ${!wttr[*]}; do kk=$((kk+3600)); date --date=@$(($(date +%s)+$kk)) +%h\ %d\ \-\ %R; printf %b "${wttr[i]}\n__\n"; done|tr -s "\n" " "|tr -s "_" "\n"|sed -e 's/^\ //g'
