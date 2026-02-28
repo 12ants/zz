cc4540138ec0aeb51ed9afbf3d70e54b2c917ede
u0_a26 <u0_a26@localhost>
2026-03-01 00:26:30 +0100

    _Xiaomi_Sun Mar  1 00:26:30 CET 2026

--- f/12calendar.sh
+++ f/12calendar.sh
@@ -4,16 +4,16 @@
 #### 
 # dott() { printf %b "\e[0m"; for i in $(seq ${1-45}); do printf %b "·"; done; printf %b "\e[0m"; }; 
 ####
-calcomp() { printf -v "epoch" %b "$(($(date +%s) / 60))"; printf -v "epcal" %b "$(($(tail -c10 $HOME/logs/calendar.json) / 60))"; printf -v "epmin" %b "$((epoch - epcal))"; }; 
+calcomp() { printf -v "epoch" %b "$(($(date +%s) / 60))"; printf -v "epcal" %b "$(($(tail -c10 $HOME/logs/calendar.json) / 60))" 2>/dev/null; printf -v "epmin" %b "$((epoch - epcal))"; }; 
 ####
 12getcal() { 
 ####
 printf %b "\e[34G getting cal "; 
-(curl -sL "https://script.google.com/macros/s/AKfycbye8O0u3we9g5Xt3HilbKzLdjlC94OwSj7QPprmzc0pWI5dZ_ORE5YSaFmlyCJ-JqBQ/exec" && printf %b "\nEPOCH_$(date +%s)") > $HOME/logs/calendar.json && printf %b "\e[G\e[K"; 
+ping -c1 -W1s ip.me &>/dev/null && (curl -sL "https://script.google.com/macros/s/AKfycbye8O0u3we9g5Xt3HilbKzLdjlC94OwSj7QPprmzc0pWI5dZ_ORE5YSaFmlyCJ-JqBQ/exec" && printf %b "\nEPOCH_$(date +%s)") > $HOME/logs/calendar.json && printf %b "\e[G\e[K"; 
 ####
 (cat $HOME/logs/calendar.json | tr -s "," "\n" | grep -vE 'EPOCH_|h_|description|end_date' | sed '/start_date_time/{s/.[0-9]*[-T]//g}' | cut -f1 -d "+" | sed -e "s/{\"summary/€\n/g"| cut -f 2- -d":" | tr -s "\n€[]" " \n"|sed -e 's/\ \"//g' -e 's/\"\ /\"/g' | col -xb | column --separator "\"" --table --output-separator " | " --table-columns "1234567890123456" --table-right 1 | tail -n+2 | bat -ppflr --theme Visual\ Studio\ Dark+ ) > $HOME/logs/cal.log; }; 
 ####
-[ -e "$HOME/logs/calendar.json" ]||12getcal; 
+[ -s "$HOME/logs/calendar.json" ]||12getcal; 
 ####
 calcomp; dott; printf %b "\e[G\e[2m${epmin} mins ago \e[0m"; 
 [[ "$epmin" -gt "88" ]] && 12getcal; 
