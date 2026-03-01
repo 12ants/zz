ff719695ae9096e549918f084ffa981f2fb01dc9
aa <aa@ae>
2026-03-01 09:58:30 +0100

    aa_ASUSTeK_Sun Mar  1 09:58:30 AM CET 2026

--- setup/fastfetch.sh
+++ setup/fastfetch.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 ## install fastfetch 
-mkdir ~/gh 2>/dev/null; 
+oldp="$PWD"; 
+mkdir ~/gh 2>/dev/null; cd ~/gh
 git clone https://github.com/fastfetch-cli/fastfetch.git; 
 cd ~/gh/fastfetch; 
 git config set remote.origin.url "git@github.com:fastfetch-cli/fastfetch.git"; 
@@ -8,6 +9,6 @@ echo run;
 . run.sh; 
 $sudo ln -s /home/$SUDO_USER/gh/build/fastfetch -t /bin/; 
 echo gg; 
-cd $OLDPWD; 
+cd $oldp; 
 ##
 ##
