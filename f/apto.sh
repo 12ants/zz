a936c526d57ce541caec4d9df4a8acebc429a776
u0_a26 <u0_a26@localhost>
2026-03-06 06:53:59 +0100

    _Xiaomi_Fri Mar  6 06:53:59 CET 2026

---
+++ f/apto.sh
@@ -0,0 +1,21 @@
+
+apto() { 
+## maybe IFS 
+
+aptoin=($(apt list --installed|grep -vE "lib.*"|cut -f1 -d"/"));
+
+aptoinver=($(apt show ${aptoin[*]} 2>/dev/null|grep -IE "Package: |Installed-Size: |Description: "|sed -e "s/Package\:\ /----\np:\ /g"|cut -f2- -d" "|tr -s "\n" " "|sed -e "s/----\ /\n/g" -e "s/\ [0-9]/\\x1b[2m&/g" -e 's/\ MB\ /\x1b[0;36mMB\x1b[0m /g' -e "s/\ kB\ /\x1b[0;34mkB\x1b[0m /g"  -e 's/\ B\ /\x1b[0;33mB\x1b[0m /g')); 
+
+aptowhen=($(cat $PREFIX/var/log/apt/history.log | grep -E "install |remove " -B1|sed -e "/[()]/d" -e "s/\ \-y//g" -e "s/apt\ //g" -e "s/.*\:\ //"|sed -e "s/install/\x1b[92m&\x1b[0m/g" -e "s/remove/\x1b[91m&\ \x1b[0m/g" | tr -s "\n" " "|sed -e "s/--\ /\n/g" -e "s/[-]/\x1b[2m&\x1b[0m/g" -e "s/[:]/\x1b[35m&\x1b[0m/g")); 
+
+apto=($(apt show ${aptoin[*]} 2>/dev/null|grep -IE "Package: |Installed-Size: |Description: "|sed -e "s/Package\:\ /----\np:\ /g"|cut -f2- -d" "|tr -s "\n" " "|sed -e "s/----\ /\n/g" -e "s/\ [0-9]/\\x1b[2m&/g" -e 's/\ MB\ /\x1b[0;36mMB\x1b[0m /g' -e "s/\ kB\ /\x1b[0;34mkB\x1b[0m /g"  -e 's/\ B\ /\x1b[0;33mB\x1b[0m /g'))
+
+printf %b "\n
+\n${aptoin[*]}
+\n${aptoinver[*]}
+\n${aptowhen[*]}
+\n${apto[*]}
+
+"; 
+}; 
+
