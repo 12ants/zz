c3dc8478e1cd7c21538d86a45edd4e002f7a565c
aa <aa@ae>
2026-03-06 11:18:38 +0100

    aa_ASUSTeK_Fri Mar  6 11:18:38 AM CET 2026

--- f/zz.weather.smhi.sh
+++ f/zz.weather.smhi.sh
@@ -8,22 +8,23 @@ wcp[0]='No precipitation' wcp[1]='Rain' wcp[2]='Thunderstorm' wcp[3]='Freezing r
 ####
 curl -sL "https://opendata-download-metfcst.smhi.se/api/category/snow1g/version/1/geotype/point/lon/16.158/lat/58.5812/data.json?timeseries=45?parameters=air_temperature,symbol_code,predominant_precipitation_type_at_surface" -o "$HOME/logs/wc.json"
 ##
-IFS=$'\n' wttr=($(cat $HOME/logs/wc.json |tr -s "[]{}, " "\n"|tail -n+10|grep -vE "inter|data|time"|cut -f2- -d":"|tr "\n" "\n"|sed -e '3~3a_' -e "s/\.00//g" -e "1~3a\ \$\{wcp\[" -e "2~3a\ \$\{wc\["  -e "2~3s/$/\]\}\,\ /g" -e "3~3s/$/\]\}/g"|tr -d "\n"|tr -s "_" "\n")); 
+local IFS=$'\n' wttr=($(cat $HOME/logs/wc.json |tr -s "[]{}, " "\n"|tail -n+10|grep -vE "inter|data|time"|cut -f2- -d":"|tr "\n" "\n"|sed -e '3~3a_' -e "s/\.00//g" -e "1~3a\ \$\{wcp\[" -e "2~3a\ \$\{wc\["  -e "2~3s/$/\]\}\,\ /g" -e "3~3s/$/\]\}/g"|tr -d "\n"|tr -s "_" "\n")); 
 
 
 # IFS=$'\n' wttr=($(cat $HOME/logs/wc.json |tr -s "[]{}, " "\n"|tail -n+10|grep -vE "inter|data|time"|cut -f2 -d":"|tr -s "\n" " "|sed -e 's/\.00/\n/g'|sed -e 's/^\ //g')); 
 ##
 # eval $(kk=0; for i in ${!wttr[*]}; do kk=$((kk+3600)); date --date=@$(($(date +%s)+$kk)) +%h\|%d\ %R; printf %b "${wttr[i]}\n__\n"; done|tr -s "\n" " "|tr -s "_" "\n"|sed -e 's/^\ //g')|bat -ppflnim --theme Nord; 
 
-eval $(kk=0; for i in ${!wttr[*]}; do kk=$((kk+3600)); date --date=@$(($(date +%s)+$kk)) +%h\|%d\ %R; printf %b "${wttr[i]}\n__\n"; done|tr -s "\n" " "|tr -s "_" "\n"|sed -e 's/^\ //g'|sed -e 's/^/echo\ \-e\ \"/g' -e 's/\ /\ \[/4' -e 's/\ /c\]\ /5' -e 's/.$/\";\ \n\n/g')|tee $HOME/logs/wc.log|bat -ppflnim --theme Nord; 
+eval $(kk=0; for i in ${!wttr[*]}; do kk=$((kk+3600)); date --date=@$(($(date +%s)+$kk)) +%h\|%d\ %R; printf %b "${wttr[i]}\n__\n"; done|tr -s "\n" " "|tr -s "_" "\n"|sed -e 's/^\ //g'|sed -e 's/^/echo\ \-e\ \"/g' -e 's/\ /\ \[/4' -e 's/\ /c\]\ /5' -e 's/.$/\";\ \n\n/g') > $HOME/logs/wc.log|bat -ppflnim --theme Nord; 
 # |tee $HOME/logs/weather.log; 
 
-alias wettr='\
-gum style --border normal --padding "1 2" --margin 2 \
-"$(head -n23 $HOME/logs/wc.log|cut -f 1- -d" "|sed -n "1~3s/$/\n/p"|bat -ppflnim --theme Nord)"'; 
+gum style --border normal --padding "1 2" --margin 2 "$(head -n23 $HOME/logs/wc.log|cut -f 1- -d" "|sed -n "1~3s/$/\n/p"|bat -ppflnim --theme Nord)"; 
 
+IFS$' \n\t'; 
 ##
 };
+
+alias wettr='zz.weather'; 
 # weather.paramaters() { 
 #### 
 # declare -a wcp[0]='No precipitation' wcp[1]='Rain' wcp[2]='Thunderstorm' wcp[3]='Freezing rain (i.e. supercooled raindrops which freeze on contact with the ground and other surfaces)' wcp[4]='Mixed/ice' wcp[5]='Snow' wcp[6]='Wet snow (i.e. snow particles which are starting to melt)' wcp[7]='Mixture of rain and snow' wcp[8]='Ice pellets' wcp[9]='Graupel' wcp[10]='Hail' wcp[11]='Drizzle' wcp[12]='Freezing drizzle (i.e. supercooled drizzle which freezes on contact with the ground and other surfaces)'; 
