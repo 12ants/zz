2200ce6295bdccc41c796604b31cac3bddf1166b
aa <aa@ae>
2026-03-06 18:16:16 +0100

    aa_ASUSTeK_Fri Mar  6 06:16:16 PM CET 2026

--- setup/grub.sh
+++ setup/grub.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 ## install grub
-sudo cp $HOME/zz/m/tard.jpg /boot/grub/tard.jpg; 
-sudo mv /etc/default/grub /etc/default/grub_$(date +%y%m%d_%H%M)
-printf '
-GRUB_BACKGROUND="/boot/grub/tard.jpg"
+$sudo cp $HOME/zz/m/tard.jpg /boot/grub/tard.jpg 2>/dev/null; 
+$sudo mv /etc/default/grub /etc/default/grub_$(date +%y%m%d_%H%M) 2>/dev/null; 
+printf 'GRUB_BACKGROUND="/boot/grub/tard.jpg"
 GRUB_MENU_BACKGROUND="/boot/grub/tard.jpg"
 GRUB_MENU_PICTURE="/boot/grub/tard.jpg"
 GRUB_GFXMODE="1024x768x32"
@@ -12,16 +11,17 @@ GRUB_DEFAULT="saved"
 GRUB_SAVEDEFAULT="true"
 GRUB_TIMEOUT_STYLE="menu"
 GRUB_TIMEOUT="4"
-GRUB_DISTRIBUTOR="`lsb_release -dcs|tail -n2|tr "\n" " " 2>/dev/null || echo Debian`"
+GRUB_DISTRIBUTOR="`lsb_release -dcs|tail -n2|tr "\n" " " 2>/dev/null  || echo Debian; df|grep -wie " /"|cut -f1 -d" "`"
 GRUB_DISABLE_OS_PROBER="false"
 GRUB_HIDDEN_TIMEOUT="0"
 GRUB_CMDLINE_LINUX="console"
 GRUB_GFXPAYLOAD_LINUX="keep"
-GRUB_COLOR_NORMAL="cyan/magenta"
-GRUB_COLOR_HIGHLIGHT="magenta/cyan"
+GRUB_COLOR_NORMAL="red/yellow"
+GRUB_COLOR_HIGHLIGHT="green/red"
 GRUB_TERMINAL_OUTPUT="gfxterm"
-GRUB_CMDLINE_LINUX_DEFAULT="console"
-' > /tmp/grub
-sudo mv /tmp/grub /etc/default/grub
-sudo update-grub; 
-# GRUB_FONT="/boot/grub/fonts/unicode.pf2"
+GRUB_CMDLINE_LINUX_DEFAULT="console"' > /tmp/grub.sh; 
+####
+####
+$sudo mv /tmp/grub.sh /etc/default/grub; 
+$sudo update-grub; 
+printf %b "\n\n $\e[96m--\e[0m GRUB updated\n\n"; 
