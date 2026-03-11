71b034fff97f74379d0d3ac6c111bb18705c9329
aa <aa@toshibalap>
2026-03-11 18:00:54 +0100

    aa_ThinkPad_Wed Mar 11 18:00:54 CET 2026

---
+++ i/codex_cli.sh
@@ -0,0 +1,25 @@
+
+
+########
+########
+########
+########
+## install vscode 
+gh repo clone microsoft/vscode
+########
+########
+## install codex-cli
+npm install -g @openai/codex
+########
+########
+## install coder 
+curl -L https://coder.com/install.sh | sh
+########
+########
+## install vs code 
+curl "https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64" \ -so "$HOME/vscode.deb"; $sudo apt install $HOME/vscode.deb; 
+########
+########
+########
+########
+
