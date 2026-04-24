bc4f1a9bfc74c3de0aee4d6ada3c1b5ffbb77a87
u0_a26 <u0_a26@localhost>
2026-04-24 11:38:12 +0200

    _Xiaomi_Fri Apr 24 11:38:12 CEST 2026

--- crons/bat.sh
+++ crons/bat.sh
@@ -2,7 +2,7 @@
 termux-battery-status > "$HOME/logs/b/battery.log"; 
 cat ~/logs/b/battery.log|grep -e "percentage"|tr -d 'A-z ,\":' | tee "$HOME/logs/b/bp.log"; 
 ####
-if [ "$(cat $HOME/logs/b/bp.log)" -lt 28 ]; then 
+if [ "$(cat $HOME/logs/b/bp.log)" -lt 12 ]; then 
 termux-tts-speak -s SYSTEM "battery at $(cat $HOME/logs/b/bp.log)%"; 
 fi; 
 ####
