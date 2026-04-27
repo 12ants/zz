04eea244fa698fae888c5efe6ba7416b9454fab9
aa <aa@aeniks.com>
2026-04-28 00:42:35 +0200

    aa_ASUSTeK_Tue Apr 28 12:42:35 AM CEST 2026

---
+++ templates/new_react_three_fiber.sh
@@ -0,0 +1,33 @@
+
+
+
+
+
+####
+
+# import { defineConfig } from "vite"
+# import react from "@vitejs/plugin-react"
+# import { r3f } from "@react-three/editor/vite"
+# 
+# export default defineConfig((env) => ({
+#   plugins: [env.command === 'build' ? react() : r3f()],
+# }))
+
+## add to ./vite.cofig.js
+
+####
+
+
+####
+####
+npm install @react-three/editor -D
+####
+npm install -g pnpm
+####
+gh repo clone pmndrs/react-three-editor; 
+cd react-three-editor; 
+####
+pnpm install; 
+pnpm run dev; 
+####
+####
