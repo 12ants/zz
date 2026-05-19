1073505ac65343f9b04c4cee7b18e6e5bf147d43
aa <aa@aeniks.com>
2026-05-19 18:06:34 +0200

    aa_ASUSTeK_Tue May 19 06:06:34 PM CEST 2026

--- new.sh
+++ new.sh
@@ -4,6 +4,7 @@ shopt -s histappend;
 shopt -s histverify; 
 ####
 export IFS=$' \n\t'; 
+[ -z "$TMUX" ] && tmux && exit;  
 [ "$TMUX" ] && tmux source $HOME/.config/tmux/tmux.conf; 
 [ "$TMUX" ] && . $HOME/zz/_ps1.sh || return 0;  
 export HISTCONTROL="ignoreboth"; export PROMPT_COMMAND="history -a; history -n; "; 
