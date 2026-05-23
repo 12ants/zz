4a05c6c4af6cfce8486f2479c90e00401d799f09
aa <aa@aants>
2026-05-23 17:34:30 +0200

    aa_ASUSTeK_Sat May 23 17:34:30 CEST 2026

--- f/webshot.sh
+++ f/webshot.sh
@@ -1,5 +1,14 @@
 webshot() {
-site="$1"; 
-echo; [ "$1" ]||read -ep 'site: ' -i 'https://' 'site'; 
+####
+site="$1"; echo $site|grep -qv 'https' && https='https://'||https=''; 
+####
+echo; unset -v scfile 2>/dev/null; 
+####
+[ "$1" ]||read -rp 'site: ' -i 'https://' 'site'; 
+####
 time="$(date +%y%m%d%H%M%S)"; 
-curl -X POST "https://api.cloudflare.com/client/v4/accounts/${CF_ACCOUNT_ID}/browser-rendering/screenshot" -H "Authorization: Bearer ${CF_API_SH}" -H 'Content-Type: application/json' -d '{ "url": "${site}'  --output "screenshot${time}_${site}.png"; echo -e "\nscreenshot written to ${PWD}/screenshot_${time}_${site}.png"; jp2a --colors --fill "${PWD}/screenshot_${time}_${site}.png" 2>/dev/null; }; 
+####
+scfile="screenshot_${time}_${site}_.png"; 
+####
+curl -X POST "https://api.cloudflare.com/client/v4/accounts/${CF_ACCOUNT_ID}/browser-rendering/screenshot" -H "Authorization: Bearer ${CF_API_SH}"  -H 'Content-Type: application/json'  -d '{ "url": "'${https}${site}'" }' --output "${scfile}"; echo -e "\nscreenshot written to ${PWD}/${scfile}"; 
+jp2a --colors -i ${scfile}; }; 
