9c19e4195f67744183e17d06a2eb7e2f9bbcd244
u0_a83 <u0_a83@localhost>
2026-05-28 21:50:32 +0200

    _Google_Thu May 28 21:50:31 CEST 2026

--- f/ai.sh
+++ f/ai.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 #### ok
-
-ai() {
-####
-prompt="$*"; [ -z "$1" ] && return 0; 
-####
-(echo --------; date; printf %b "#### prompt: \n$prompt\n#### answer: \n") | tee -a ~/logs/ailog.log; 
-####
-curl -sL "https://generativelanguage.googleapis.com/v1beta/models/gemini-flash-latest:generateContent" -H 'Content-Type: application/json' -H "X-goog-api-key: ${GOOGLE_API_KEY}" -X POST -d '{"contents": [ {"parts": [ {"text": "'"${prompt}"'" } ] } ] }'|sed -e "/thoughtSignature/d"|grep -e "text"|cut -f2- -d":"|sed -e "s/..$//" -e "s/^..//"|tee -a ~/logs/ailog.log; 
-####
-}; 
+function ai () { printf %b "\n\n\e[A"; mkdir -p ~/logs/ai 2>dev/null; time="$(date +%T)"; prompt="$*"; [ -z "$1" ] && (echo "ai: "; read -e "prompt"); ( echo --------; date; printf %b "#### prompt: \n$prompt\n#### answer: \n" ) | tee -a ~/logs/ailog.log; printf %b "$(curl -sL "https://generativelanguage.googleapis.com/v1beta/models/gemini-flash-latest:generateContent" -H 'Content-Type: application/json' -H "X-goog-api-key: ${GOOGLE_API_KEY}" -X POST -d '{"contents": [ {"parts": [ {"text": "'"${prompt}"'" } ] } ] }' )" | grep -E '"text"|"modelVersion"|"message"' | tac|cut -f2- -d":" | sed -e "3s/..$//" -e "3s/^..//"|tee -a ~/logs/ai/ai_${time}_.md|bat -pflmd; };
 
 
 
