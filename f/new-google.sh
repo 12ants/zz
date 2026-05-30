e1439428de1e463e50a03d2afdef92432d9a2c73
u0_a26 <u0_a26@localhost>
2026-05-30 19:14:36 +0200

    _Xiaomi_Sat May 30 19:14:36 CEST 2026

--- f/new-google.sh
+++ f/new-google.sh
@@ -6,11 +6,10 @@ pos="${HOME}/logs/news";
 mkdir -p ${pos} 2>/dev/null; 
 time="$(date +%y%m%d%H%M%S)"; 
 hash lynx 2>/dev/null || $sudo apt in lynx -y 2>/dev/null; 
-# /google_${sub}_${time}_"; 
 ##
-curl -sL "https://news.google.com/rss/search?q=${sub}" > ${pos}/google_${sub}_${time}_.xml
+curl -sL "https://news.google.com/rss/search?q=${sub}" > ${pos}/google_${sub}_${time}_.xm
 ##
-cp ${pos}/temp.html ${pos}/google_${sub}_${time}_.html; 
+printf '<!DOCTYPE html><html lang="en"><head><meta charset="utf-8" /><meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/><title>hello</title><style>body {font-family: monospace;font-size: 1em;width: 100vw;height: 100%;margin: 2px;overflow-x: hidden;justify-content: space-between;display: flex;flex-direction: row;flex-wrap: wrap;}a{padding: 2px;text-decoration: underline dashed #44bbffcc;color: #004466;font-weight: 600;}pre { white-space: pre-line; width: fit-content; margin: 4px 22px 22px 4px; text-decoration: none; outline: solid 2px #222222; border-bottom: solid 4px; padding: 4px; background: #ff228822;}</style></head><body>' > ${pos}/google_${sub}_${time}_.html; 
 # 
 cat ${pos}/google_${sub}_${time}_.xml | sed -e "s/<*>/&\n/g" | sed -n "/<pubDate>/,/<\/description>/p"|sed -e "s/\&lt\;/</g" -e "s/\&gt\;/>/g" -re 's/(nbsp|&amp)\;//g' -e "s/<description>/<pre>/g" -e "s/<\/description>//g" -e "s/<pubDate>/\ -\ /g" -e "s/\/pubDate>/\/pre>____/g" -e "1,2d"|tr -d "\n"|sed -e "s/____/\n\n/g" >> ${pos}/google_${sub}_${time}_.html; 
 ##
