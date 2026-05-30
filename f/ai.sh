6c59ae5707c69d87cab59ad740f5731b643f27d4
u0_a26 <u0_a26@localhost>
2026-05-30 19:09:40 +0200

    _Xiaomi_Sat May 30 19:09:40 CEST 2026

--- f/ai.sh
+++ f/ai.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 #### ok
-function ai () { printf %b "\n\n\e[A"; mkdir -p ~/logs/ai 2>/dev/null; time="$(date +%T)"; prompt="$*"; [ -z "$1" ] && (echo -n "ai: "; read -e "prompt"); ( echo --------; date; printf %b "#### prompt: \n$prompt\n#### answer: \n" ) | tee -a ~/logs/ailog.log; 
+function ai () { 
+#!/bin/bash
+###########
+###########
+local IFS=$' \n\t'; 
+printf %b "\n\n\e[A"; mkdir -p ~/logs/ai 2>/dev/null; time="$(date +%T)"; prompt="$*"; [ -z "$1" ] && (echo -n "ai: "; read -e "prompt"); ( echo --------; date; printf %b "#### prompt: \n$prompt\n#### answer: \n" ) | tee -a ~/logs/ailog.log; 
 ####
 printf %b "$(curl -sL "https://generativelanguage.googleapis.com/v1beta/models/gemini-flash-latest:generateContent" -H 'Content-Type: application/json' -H "X-goog-api-key: ${GOOGLE_API_KEY}" -X POST -d '{"contents": [ {"parts": [ {"text": "'"${prompt}"'" } ] } ] }' )" \
 | grep -E '"text"|"modelVersion"|"message"' | tac|cut -f2- -d":" | sed -e "3s/..$//" -e "3s/^..//"|tee -a ~/logs/ai/ai_${time}_.md|bat -pflmd; 
@@ -9,11 +14,11 @@ printf %b "$(curl -sL "https://generativelanguage.googleapis.com/v1beta/models/g
 
 
 
-_ai () {
-####
-####
+ai_ () {
 #!/bin/bash
-IFS=$' \n\t'; 
+###########
+###########
+local IFS=$' \n\t'; 
 printf %b "\n\n\e[A"; mkdir -p ~/logs/ai 2>/dev/null; time="$(date +%T)"; 
 prompt="$*"; if [ -z "$1" ]; then echo -n "ai: "; read -re "prompt"; 
 fi; printf %b "$(date)\n#### prompt: \n$prompt\n#### answer: \n" | tee -a ~/logs/ailog.log; 
