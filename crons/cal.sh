ea36c3d526239977454d102268213a964827195a
aa <aa@aants>
2026-06-01 01:12:43 +0200

    aa_ASUSTeK_mån  1 juni 2026 01:12:43 CEST

--- crons/cal.sh
+++ crons/cal.sh
@@ -1,1 +1,19 @@
-(curl -sL "https://script.google.com/macros/s/AKfycbwSft4XmNq-lCW38uuBjihWM8pKMGrm-1uDmwusW7uksz0uN3WIEobzOt-0NQUgDOASqQ/exec" && printf %b "\nEPOCH_"; date +%s|tr -d "\n") > $HOME/logs/calendar.json; 
+#!/bin/bash
+#### getg g
+calcomp() { printf -v "epoch" %b "$(($(date +%s) / 60))"; printf -v "epcal" %b "$(($(tail -c10 $HOME/logs/calendar.json) / 60))" 2>/dev/null; printf -v "epmin" %b "$((epoch - epcal))"; }; 
+####
+getcal() { (curl -sL "https://script.google.com/macros/s/AKfycbwSft4XmNq-lCW38uuBjihWM8pKMGrm-1uDmwusW7uksz0uN3WIEobzOt-0NQUgDOASqQ/exec" && printf %b "\nEPOCH_"; date +%s|tr -d "\n") > $HOME/logs/calendar.json && (cat $HOME/logs/calendar.json | tr -s "," "\n" | grep -vE 'EPOCH_|h_|description|end_date' | sed '/start_date_time/{s/.[0-9]*[-T]//g}' | cut -f1 -d "+" | sed -e "s/{\"summary/€\n/g"| cut -f 2- -d":" | tr -s "\n€[]" " \n"|sed -e 's/\ \"//g' -e 's/\"\ /\"/g' | col -xb | column --separator "\"" --table --output-separator " | " --table-columns "123456789012345678900" --table-right 1| tail -n+2 | bat -ppflr --theme Visual\ Studio\ Dark+ ) > $HOME/logs/cal.log; }; 
+####
+####
+[ -s "$HOME/logs/calendar.json" ] && getcal; 
+####
+calcomp; 
+####
+[[ "$epmin" -gt "88" ]] && getcal; 
+calcomp; 
+####
+[ -e $HOME/logs/calendar.json ] || 12getcal; 
+cat $HOME/logs/calendar.json|grep -e "EPOCH" --quiet || 12getcal; 
+
+
+for i in {1..28}; do printf %b "·"; done; 
