b24fa578bf2b135943772e7fcb16d6cf19fb99ba
aa <aa@think>
2026-06-27 20:34:05 +0200

    aa_ThinkPad_Sat Jun 27 08:34:05 PM CEST 2026

--- setup/aichat.sh
+++ setup/aichat.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 ## dl aichat
-if [ "$PREFIX" ]; then apt install aichat -y; else 
-wget -L "https://github.com/sigoden/aichat/releases/download/v0.30.0/aichat-v0.30.0-x86_64-unknown-linux-musl.tar.gz"; 
-tar -xvf ./aichat-v0.30.0-x86_64-unknown-linux-musl.tar.gz; 
-rm ./aichat-v0.30.0-x86_64-unknown-linux-musl.tar.gz; 
-mv ./aichat -t $HOME/bin/; 
+if [ "${PREFIX}" ]; then apt install aichat -y; else wget -L "https://github.com/sigoden/aichat/releases/download/v0.30.0/aichat-v0.30.0-x86_64-unknown-linux-musl.tar.gz"; tar -xvf ./aichat-v0.30.0-x86_64-unknown-linux-musl.tar.gz; rm ./aichat-v0.30.0-x86_64-unknown-linux-musl.tar.gz; mv ./aichat -t $HOME/bin/; fi; 
 ####
-fi; 
 mkdir $HOME/.config/aichat 2>/dev/null; 
-ln -s $HOME/zz/c/aichat/* -t $HOME/.config/aichat
-gpg --pinentry loopback -o $HOME/.config/aichat/config.yaml -d $HOME/.config/aichat/config.yaml.gpg; 
+[ -e "${HOME}/.safe/pw.sh" ] && \
+gpg --pinentry loopback --passphrase-file "${HOME}/.safe/pw.sh" -o "${HOME}/.config/aichat/config.yaml" -d "${HOME}/zz/setup/gpg/aichat.config.yaml.gpg" || \
+gpg --pinentry loopback -o "${HOME}/.config/aichat/config.yaml" -d "${HOME}/zz/setup/gpg/aichat.config.yaml.gpg"; 
 ####
