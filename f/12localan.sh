8d7a04bb8785e6e8f53c87618a7929a3ddfe0e05
u0_a104 <u0_a104@localhost>
2026-06-28 01:09:14 +0200

    _Google_Sun Jun 28 01:09:14 CEST 2026

--- f/12localan.sh
+++ f/12localan.sh
@@ -3,12 +3,12 @@
 12localan() { 
 ## discover lan w ping & ssh 
 #ipt=$(mktemp; ); 
-mkdir -p $HOME/logs/iplocal/ 2>/dev/null; 
-rm $HOME/logs/iplocal/* 2>/dev/null;  
+mkdir -p $HOME/logs/zz/iplocal/ 2>/dev/null; 
+rm $HOME/logs/zz/iplocal/* 2>/dev/null;  
 c5='    \e[96m--\e[0m'; 
 c6='\e[96m--\e[0m'; 
-wlan="$(cat $HOME/logs/wlan.log)"; 
-iploc="$(cat $HOME/logs/wlan.log)"; 
+wlan="$(cat $HOME/logs/zz/wlan.log)"; 
+iploc="$(cat $HOME/logs/zz/wlan.log)"; 
 # printf %b "\n\n\n\n\e[4A\n\e[96m --\n\e[0m "; 
 # read -ep '> ' -i "${wlan[*]}" "iploc"; 
 printf %b "\e[?25l\n$c5 checking ips\e[2m ...\e[0m \t\t [q]uit $c6\n\n\n\n\e[4A\n\n"; 
@@ -17,8 +17,8 @@ for ip in {0..266}; do printf %b "\e[K";
 if ping -c2 ${iploc%.*}.${ip} &>/dev/null; 
 then ipp="${iploc%.*}.${ip}"; 
 printf %b "\e[K\n\n$ipp\e[K\e[A\e[K"; 
-touch $HOME/logs/iplocal/$ipp; 
-(nmap ${ipp} --open -p 22,8022 2>/dev/null|grep -e "open"|cut -f1 -d"/"|tr -s "\n" " ") > $HOME/logs/iplocal/${ipp} 
+touch $HOME/logs/zz/iplocal/$ipp; 
+(nmap ${ipp} --open -p 22,8022 2>/dev/null|grep -e "open"|cut -f1 -d"/"|tr -s "\n" " ") > $HOME/logs/zz/iplocal/${ipp} 
 fi & disown; 
 # printf %b "\e[K\n${iploc%.*}.$i" && 
 printf %b "\e[K\e[A\e[K"; 
@@ -27,23 +27,23 @@ read -t 0.05 -n1 -s -p "${iploc%.*}.$ip " "ny"; [ $ny ] && printf %b "\n\n\e[?25
 printf %b "\e[?25h";  
 printf %b "\e[K\n----------------\n\n"; 
 # ipends=($(cat $ipt|cut -f4 -d"."; ));
-rm $HOME/logs/iplocal/$iploc 2>/dev/null; 
-iplocal=($(command ls $HOME/logs/iplocal; )); 
+rm $HOME/logs/zz/iplocal/$iploc 2>/dev/null; 
+iplocal=($(command ls $HOME/logs/zz/iplocal; )); 
 # iplocall=-($iploc);
-# command ls "$HOME/logs/iplocal"; echo; 
+# command ls "$HOME/logs/zz/iplocal"; echo; 
 
-# for ip in ${iplocal[*]}; do (nmap --open $ipp -p 22,8022 --system-dns 2>/dev/null|grep -e "open"|cut -f1 -d"/"|tr -s "\n" " ") > $HOME/logs/iplocal/$ipp; done; 
+# for ip in ${iplocal[*]}; do (nmap --open $ipp -p 22,8022 --system-dns 2>/dev/null|grep -e "open"|cut -f1 -d"/"|tr -s "\n" " ") > $HOME/logs/zz/iplocal/$ipp; done; 
 
 printf %b "\e[?25h"; 
 ## selection menu 
 lomenu() { 
 local IFS=$'\n\t '; 
 
-iplogs=$HOME/logs/iplocal;
+iplogs=$HOME/logs/zz/iplocal;
 ops=($2); [ "$2" ] || \
 ops=($(for i in  $(command ls -1 $iplogs); do [ "$(command ls ${iplogs}/${i} -s|cut -c1-2)" -gt 0 ] && printf %b "$i:$(cat $iplogs/$i)\n"; done));
 local prompt="$1" index="0" cur="0" count="${#ops[@]}";  
-# ops=($(command ls -1 $HOME/logs/iplocal)); 
+# ops=($(command ls -1 $HOME/logs/zz/iplocal)); 
 # local desc=()
 for i in "${!ops[*]}"; do echo; done; printf %b "\e[${#ops[*]}A"; 
 printf "\e[?25l$c5 $prompt $c6\n"; ## print prompt
@@ -77,7 +77,7 @@ printf -v sel "${ops[$cur]}";
 printf %b "\e[?25h\n\e[A$c5 user: "; 
 read -re -i "aa" "u"; [ -z $u ]&& u=$UID; 
 printf %b "\e[?25h$c5 connecting to: \e[7m ${u}@${sel} \e[0m\n\n"; 
-# -p $(cat $logs/$o) 
+# -p $(cat $logs/zz/$o) 
 echo; 
 sshlatest="${sel/:*/} -l $u -p ${sel/*:/}"; 
 printf %b "${sshlatest}"|tr -s "\n" " " > $logs/$sel; 
